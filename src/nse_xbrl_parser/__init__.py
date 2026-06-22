cb097329b86d66fec47b3f3e5fb2c43749d3ada7
eggmasonvalue <kirubhasshankarkbs@gmail.com>
2026-06-22 21:53:34 +0530

    feat: add context-preserving parse_xbrl_facts extractor (#3)

--- src/nse_xbrl_parser/__init__.py
+++ src/nse_xbrl_parser/__init__.py
@@ -1,4 +1,4 @@
-from .parser import parse_xbrl_file
+from .parser import parse_xbrl_facts, parse_xbrl_file
 
-__version__ = "0.1.0"
-__all__ = ["parse_xbrl_file"]
+__version__ = "0.2.0"
+__all__ = ["parse_xbrl_file", "parse_xbrl_facts"]
